48eb66544d816375ef2f4f3a6e472de42a94b045
LuanEduardoS <luaneduardolima186@gmail.com>
2022-12-13 15:33:59 -0300

    .
    baixei o React e fiz o exercício

---
+++ 1_INTRO/hello-word/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Word React!</h1>
+      <p>Estou adorando React</p>
+    </div>
+  );
+}
+
+export default App;
+
