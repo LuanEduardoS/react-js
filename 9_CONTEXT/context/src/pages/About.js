0de37b880d1e90b8d670aa67c92711496d079c9d
LuanEduardoS <luaneduardolima186@gmail.com>
2023-03-19 23:04:24 -0300

    fim do móduco de context API

---
+++ 9_CONTEXT/context/src/pages/About.js
@@ -0,0 +1,24 @@
+//import { useContext } from "react"
+//import { CounterContext } from "../context/CounterContext"
+
+import { useCounterContext } from "../hooks/useCounterCounter"
+
+import { useTitleColorContext } from "../hooks/useTitleColorContext"
+
+const About = () => {
+    //const { counter } = useContext(CounterContext)
+    const { counter } = useCounterContext()
+
+    // 5 - context mais complexo 
+    const {color} = useTitleColorContext()
+
+
+  return (
+    <div>
+        <h1 style={{ color: color}}>About</h1>
+        <p>Valor do contador: {counter}</p>
+    </div>
+  )
+}
+
+export default About
