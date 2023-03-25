f6796fa0af6d05d3dda13c13f18024d88bfc0252
LuanEduardoS <luaneduardolima186@gmail.com>
2023-03-25 14:17:39 -0300

    miniblog 119

---
+++ 10_MINIBLOG/miniblog/src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDQ9Zxq9CCvC9UL-ROnM51sIS30Rckia7I",
+  authDomain: "miniblog-e73b4.firebaseapp.com",
+  projectId: "miniblog-e73b4",
+  storageBucket: "miniblog-e73b4.appspot.com",
+  messagingSenderId: "64383155212",
+  appId: "1:64383155212:web:407a712509e5d64051c229"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app)
+
+export { db }
