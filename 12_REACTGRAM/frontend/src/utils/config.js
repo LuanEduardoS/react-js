8386282dbfa6328febc66bf51e6109ed0533f41c
LuanEduardoS <luaneduardolima186@gmail.com>
2023-05-02 22:46:20 -0300

    avançando no frontend

--- 12_REACTGRAM/frontend/src/utils/config.js
+++ 12_REACTGRAM/frontend/src/utils/config.js
@@ -2,11 +2,11 @@ export const api = "http://localhost:5000/api";
 export const uploads = "http://localhost:5000/uploads";
 
 export const requestConfig = (method, data, token = null, image = null) => {
-  let config;
+  let config
 
   if (image) {
     config = {
-      method: method,
+      method,
       body: data,
       headers: {},
     };
@@ -17,7 +17,7 @@ export const requestConfig = (method, data, token = null, image = null) => {
     };
   } else {
     config = {
-      method: method,
+      method,
       body: JSON.stringify(data),
       headers: {
         "Content-Type": "application/json",
