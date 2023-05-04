a2b2be9d225701c84afcaffc93d03d04b492c977
LuanEduardoS <luaneduardolima186@gmail.com>
2023-05-04 12:50:36 -0300

    Finalizando função de excluir foto

--- 12_REACTGRAM/frontend/src/pages/Profile/Profile.js
+++ 12_REACTGRAM/frontend/src/pages/Profile/Profile.js
@@ -14,7 +14,7 @@ import { useParams } from "react-router-dom";
 
 // redux
 import { getUserDetails } from "../../slices/userSlice";
-import { publishPhoto, resetMessage, getUserPhotos } from "../../slices/photoSlice";
+import { publishPhoto, resetMessage, getUserPhotos, deletePhoto } from "../../slices/photoSlice";
 
 const Profile = () => {
     const {id} = useParams()
@@ -47,6 +47,12 @@ const Profile = () => {
         setImage(image);
       };
 
+      const resetComponentMessage = () => {
+        setTimeout(() => {
+            dispatch(resetMessage())
+        }, 2000)
+      }
+
     const submitHandle = (e) => {
         e.preventDefault();
 
@@ -68,9 +74,13 @@ const Profile = () => {
 
         setTitle("")
 
-        setTimeout(() => {
-            dispatch(resetMessage())
-        }, 2000)
+        resetComponentMessage()
+    }
+
+    // Delete a photo
+    const handleDelete = (id) => {
+        dispatch(deletePhoto(id))
+        resetComponentMessage()
     }
 
     if (loading) {
@@ -133,7 +143,7 @@ const Profile = () => {
                                         <BsFillEyeFill />
                                     </Link>
                                     <BsPencilFill />
-                                    <BsXLg />
+                                    <BsXLg onClick={() => handleDelete(photo._id)} />
                                 </div>
                             ) : (
                                 <Link className="btn" to={`/photos/${photo._id}`}>
