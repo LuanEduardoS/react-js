061f7d9a835781b27f9fb8450dd04575ca7c2583
LuanEduardoS <luaneduardolima186@gmail.com>
2023-05-07 22:36:08 -0300

    fim do css do modal

--- 17_todo_react/src/App.tsx
+++ 17_todo_react/src/App.tsx
@@ -5,12 +5,14 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
+import Modal from "./components/Modal";
 
 // CSS
 import styles from "./App.module.css"
 
 // Interface 
 import { ITask } from "./interfaces/Task";
+
 
 function App() {
   const [taskList, setTaskList] = useState<ITask[]>([])
@@ -25,6 +27,7 @@ function App() {
 
   return (
     <div>
+      <Modal children={<TaskForm btnText="Editar Tarefa" taskList={taskList}/>}/> 
       <Header />
         <main className={styles.main}>
         <div>
