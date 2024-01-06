4c10f4257264a6800257535aed4e61de1f8b2a2e
LuanEduardoS <luaneduardolima186@gmail.com>
2024-01-06 17:59:28 -0300

    criação de projetos com Vite e React

--- 1_INTRO/hello-word/src/App.js
+++ 1_INTRO/hello-word/src/App.js
@@ -1,14 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Word React!</h1>
-      <p>Estou adorando React</p>
+      <h1>Hello World React!</h1>
     </div>
   );
 }
 
 export default App;
-
