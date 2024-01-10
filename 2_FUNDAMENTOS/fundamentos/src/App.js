f22266c0b81e2ecaaaeef9bbd82fd2f2f8e4d099
LuanEduardoS <luaneduardolima186@gmail.com>
2024-01-10 17:31:55 -0300

    revisão até template expressions

--- 2_FUNDAMENTOS/fundamentos/src/App.js
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+// components
+import FirstComponent from './components/FirstComponent';
+import TemplateExpressions from './components/TemplateExpressions';
 
+// styles / CSS
+import './App.css';
 function App() {
   return (
     <div className="App">
       <h1>Fundamentos React</h1>
+      <FirstComponent/>
+      <TemplateExpressions/>
     </div>
   );
 }
---
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -0,0 +1,14 @@
+// arquivo de estilo
+
+const FirstComponent = () => {
+    // essa função faz isso
+    return (
+        <div>
+            {/*Algum comentário*/}
+            <h1>Meu primeiro componente</h1>
+            <p className="teste">Meu Texto</p>
+        </div>
+    )
+}
+
+export default FirstComponent
