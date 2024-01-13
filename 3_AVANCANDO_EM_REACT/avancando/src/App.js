4ddc49e882fa3bf065cc3669ee30549ad1f62deb
LuanEduardoS <luaneduardolima186@gmail.com>
2024-01-13 17:14:24 -0300

    revisão da parte avançada até previous state

--- 3_AVANCANDO_EM_REACT/avancando/src/App.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.js
@@ -1,104 +1,24 @@
-import { useState } from 'react';
-import './App.css';
-
-import City from "./assets/city.jpg";
-import CarDetails from './components/CarDetails';
-import ConditionalRender from './components/ConditionalRender';
-import ListRender from './components/ListRender';
-import ManageData from './components/ManageData';
-import ShowUserName from './components/ShowUserName';
-import Fragment from './components/Fragment';
-import Container from './components/Container';
-import ExecuteFunction from './components/ExecuteFunction';
-import Message from './components/Message';
-import ChangeMessageState from './components/ChangeMessageState';
-import UserDetails from './components/UserDetails';
-
-
+import "./App.css";
+import City from "./assets/city.jpg"
+import ManageData from "./components/ManageData";
+import ListRender from "./components/ListRender";
 
 function App() {
-  //const name = "Joaquim"
-  const [userName] = useState("Maria")
-  
-  const cars = [
-    {id: 1, brand: "Ferrari", color: "Amarela", newCar: true, km:0 },
-    {id: 2, brand: "KIA", color:"Branco", newCar:false, km:34343 },
-    {id: 3, brand: "Renault", color:"Azul", newCar:false, km: 234 },
-  ];
-
-  function showMessage() {
-    console.log("Evento do componente pai")
-  }
-
-  const [message, setMessage] = useState("");
-  const handleMessage = (msg) => {
-    setMessage(msg);
-  }
-
-  const users = [
-    {id: 1, name: "Matheus", job: "Proramador", age: 31 },
-    {id: 2, name: "Maria", job:"Advogada", age:31 },
-    {id: 3, name:"Josias", job:"Reporter", age: 66},
-    {id: 4, name:"Miguel", job:"Estudante", age: 16},
-  ];
-
   return (
     <div className="App">
       <h1>Avançando em React</h1>
-      {/* Imagem em public */}
+      {/* Imagem em React */}
       <div>
         <img src="/img1.jpg" alt="Paisagem" />
       </div>
+      {/* Imagem em asset */}
       <div>
-        {/* Imagem em asset*/}
-        <img src={City} alt="Cidade" />
+          <img src={City} alt="Cidade" />
       </div>
-      <ManageData />
-      <ListRender />
-      <ConditionalRender />
-      {/* props */}
-      <ShowUserName name={userName} />
-      {/* destructuring */}
-      <CarDetails brand="VW" km= {100000} color="Azul" newCar={false} />
-      {/* reaproveitando*/}
-      <CarDetails brand = "Ford" color = "Vermelha" km={0} newCar={true} />
-      <CarDetails brand = "Fiat" color = "Branco" km={4500} newCar={false} />
-      {/* loop em array de objetos */}
-      {cars.map((car) => (
-        <CarDetails
-        key={car.id}
-        brand={car.brand}
-        color={car.color}
-        km={car.km}
-        newCar={car.newCar}
-        />
-      ))}
-      {/*fragment*/}
-      <Fragment propFragment="teste" />
-      {/* children */}
-      <Container myValue="testing">
-        <p>E este é o contúdo</p>
-      </Container>
-      <Container myValue="testing 2">
-        <h5>Testando o conteúdo</h5>
-      </Container>
-      {/* executar função */}
-      <ExecuteFunction myFunction= {showMessage}/>
-      {/* state lift */}
-      <Message msg={message} />
-      <ChangeMessageState handleMessage={handleMessage} />
-      {/*Desafios*/}
-      {users.map((user) => (
-      <UserDetails
-        key={user.id}
-        name={user.name}
-        job={user.job}
-        age={user.age}
-      />
-      )
-      )}
-     </div>
-  );
+      <ManageData/>
+      <ListRender/>
+    </div>
+  )
 }
 
-export default App;
+export default App
--- 3_AVANCANDO_EM_REACT/avancando/src/components/ManageData.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/components/ManageData.js
@@ -1,24 +1,26 @@
-import { useState } from "react";
+import {useState} from "react";
 
 const ManageData = () => {
-  let someData= 10;
-  
-  console.log(someData);
-  
-  const [number, setNumber] = useState(15);
+  let someData = 10;
+
+  console.log(someData)
+
+  const [number, setNumber] = useState(15)
+
+  console.log(number)
 
   return (
     <div>
       <div>
-        <p>valor: {someData}</p>
-        <button onClick={() => (someData = 15)}>Mudar variável</button>
+        <p>Valor: {someData}</p>
+        <button onClick={() => (someData = 15)}>Mudar Variável</button>
       </div>
       <div>
         <p>Valor: {number}</p>
         <button onClick={() => setNumber(25)}>Mudar o state</button>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ManageData;
+export default ManageData
