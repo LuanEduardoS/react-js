069814de5b2989ef2b6a0b42dff0b738779aaf6b
LuanEduardoS <luaneduardolima186@gmail.com>
2024-03-05 22:25:46 -0300

    revisão até a aula 23

--- 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -1,17 +1,9 @@
-// arquivo de estilo
-
-import MyComponent from "./MyComponent"
-
 const FirstComponent = () => {
-    // essa função faz isso
     return (
         <div>
-            {/*Algum comentário*/}
             <h1>Meu primeiro componente</h1>
-            <p className="teste">Meu Texto</p>
-            <MyComponent/>
         </div>
     )
 }
 
-export default FirstComponent
+export default FirstComponent;
