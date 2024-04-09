e164e5cb694e9f22727aad9b2cf330b23152f935
LuanEduardoS <luaneduardolima186@gmail.com>
2024-04-09 12:08:03 -0300

    algumas alterações

--- 2_FUNDAMENTOS/fundamentos/src/App.js
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 
+import TemplateExpressions from './components/TemplateExpressions';
 function App() {
   return (
     <div className="App">
       <h1>Fundamentos React</h1>
+      <TemplateExpressions/>
     </div>
   );
 }
